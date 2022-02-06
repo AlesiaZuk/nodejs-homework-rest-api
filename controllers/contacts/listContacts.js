c9ee675ccb9b8d8c44856256e543495e509e2ce6
AlesiaZuk <78986532+AlesiaZuk@users.noreply.github.com>
2022-02-06 15:13:38 +0200

    favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -5,12 +5,16 @@ const {
 const listAllContacts = async (req, res, next) => {
   try {
     const { _id } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10, favorite = [true, false] } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner: _id }, "-createdAt -updatedAt", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "_id email");
+    const result = await Contact.find(
+      { owner: _id, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit: Number(limit),
+      }
+    ).populate("owner", "_id email");
     res.json(result);
   } catch (error) {
     next(error);
